7b616693cc728b74586dbfe50ea328b00fc40f64
jk-jumur <developer.jumur@gmail.com>
2026-09-01 02:05:46 +0600

    Setup Better Auth for Authentication, Introduction to React Hook Form

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
+import Statistics from "@/components/Statistics";
+import Testimonials from "@/components/Testimonials";
+import WhyChoose from "@/components/WhyChoose";
+
 
 export default function Home() {
+  const stats = {
+      totalEvents: 30,
+      totalAtteends:4000,
+      totalOrgs: 10
+
+  }
   return (
    <>
-     <h2>home page</h2>
+     <Hero/>
+     
+     <WhyChoose/>
+     <Statistics stats={stats}/>
+     <Testimonials/>
    </>
   );
 }
